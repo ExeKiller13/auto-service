d70044e60480be2d3beb4a401081ee1cf62dfb23
Alokhin <alokhin.kirill@gmail.com>
2018-01-14 22:06:02 +0200

    created application skeleton

---
+++ src/main/java/com/auto/service/autoservice/AutoServiceApplication.java
@@ -0,0 +1,12 @@
+package com.auto.service.autoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoServiceApplication.class, args);
+	}
+}
