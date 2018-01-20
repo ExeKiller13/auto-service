db673a3e3ae3875830c8ff5e298d93cba51c4703
Alokhin <alokhin.kirill@gmail.com>
2018-01-20 14:18:12 +0200

    added password reset token entity

---
+++ src/main/java/com/alokhin/autoservice/persistence/model/entity/PasswordResetTokenEntity.java
@@ -0,0 +1,49 @@
+package com.alokhin.autoservice.persistence.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Calendar;
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table (name = "password_reset_token")
+public class PasswordResetTokenEntity {
+    private static final int EXPIRATION = 60 * 24;
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String token;
+
+    @OneToOne (targetEntity = AccountEntity.class, fetch = FetchType.EAGER)
+    @JoinColumn (nullable = false, name = "account_id")
+    private AccountEntity accountEntity;
+
+    @Builder.Default
+    private Date expiryDate = calculateExpiryDate(EXPIRATION);
+
+    public PasswordResetTokenEntity(String token, AccountEntity accountEntity) {
+        this.token = token;
+        this.accountEntity = accountEntity;
+    }
+
+    private static Date calculateExpiryDate(int expiryTimeInMinutes) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTimeInMillis(new Date().getTime());
+        cal.add(Calendar.MINUTE, expiryTimeInMinutes);
+        return new Date(cal.getTime().getTime());
+    }
+
+    public void updateToken(String token) {
+        this.token = token;
+        this.expiryDate = calculateExpiryDate(EXPIRATION);
+    }
+}
