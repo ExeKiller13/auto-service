0fe79bd9c7720a7a3c048e4410695a1935f921cf
Alokhin <alokhin.kirill@gmail.com>
2018-01-21 15:17:39 +0200

    added password reset token not found exception

---
+++ src/main/java/com/alokhin/autoservice/exception/PasswordResetTokenNotFoundException.java
@@ -0,0 +1,8 @@
+package com.alokhin.autoservice.exception;
+
+public class PasswordResetTokenNotFoundException extends Exception {
+
+    public PasswordResetTokenNotFoundException(String message) {
+        super(message);
+    }
+}
