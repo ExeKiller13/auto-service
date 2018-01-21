15d693fa2dd389d7c1adfeeb830346c53bbc5ece
Alokhin <alokhin.kirill@gmail.com>
2018-01-21 12:59:38 +0200

    refactored

--- src/main/java/com/alokhin/autoservice/security/config/SecurityConfig.java
+++ src/main/java/com/alokhin/autoservice/security/config/SecurityConfig.java
@@ -49,7 +49,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService)
-            .passwordEncoder(new ShaPasswordEncoder(encodingStrength));
+            .passwordEncoder(new ShaPasswordEncoder(encodingStrength.intValue()));
     }
 
     @Override
