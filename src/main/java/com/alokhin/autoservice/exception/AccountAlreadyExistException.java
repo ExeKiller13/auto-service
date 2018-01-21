ab317ede1b3b8b767d3f63e7c6d1cd28dffd0c41
Alokhin <alokhin.kirill@gmail.com>
2018-01-21 22:47:02 +0200

    added account already exist exception

---
+++ src/main/java/com/alokhin/autoservice/exception/AccountAlreadyExistException.java
@@ -0,0 +1,8 @@
+package com.alokhin.autoservice.exception;
+
+public class AccountAlreadyExistException extends Exception {
+
+    public AccountAlreadyExistException(String message) {
+        super(message);
+    }
+}
