71e3a0a08dd0952a1b9e294097d0bb9e775e8709
Alokhin <alokhin.kirill@gmail.com>
2018-01-21 15:45:55 +0200

    implemented password reset token service

---
+++ src/main/java/com/alokhin/autoservice/service/PasswordResetTokenService.java
@@ -0,0 +1,14 @@
+package com.alokhin.autoservice.service;
+
+import com.alokhin.autoservice.exception.PasswordResetTokenNotFoundException;
+import com.alokhin.autoservice.persistence.model.entity.AccountEntity;
+import com.alokhin.autoservice.persistence.model.entity.PasswordResetTokenEntity;
+
+public interface PasswordResetTokenService {
+
+    PasswordResetTokenEntity findByToken(String token) throws PasswordResetTokenNotFoundException;
+
+    PasswordResetTokenEntity findByAccountEntity(AccountEntity accountEntity) throws PasswordResetTokenNotFoundException;
+
+    PasswordResetTokenEntity save(PasswordResetTokenEntity entity);
+}
---
+++ src/main/java/com/alokhin/autoservice/service/impl/PasswordResetTokenServiceImpl.java
@@ -0,0 +1,43 @@
+package com.alokhin.autoservice.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.alokhin.autoservice.exception.PasswordResetTokenNotFoundException;
+import com.alokhin.autoservice.persistence.dao.PasswordResetTokenRepository;
+import com.alokhin.autoservice.persistence.model.entity.AccountEntity;
+import com.alokhin.autoservice.persistence.model.entity.PasswordResetTokenEntity;
+import com.alokhin.autoservice.service.PasswordResetTokenService;
+
+@Service
+public class PasswordResetTokenServiceImpl implements PasswordResetTokenService {
+
+    private final PasswordResetTokenRepository passwordResetTokenRepository;
+
+    @Autowired
+    public PasswordResetTokenServiceImpl(PasswordResetTokenRepository passwordResetTokenRepository) {
+        this.passwordResetTokenRepository = passwordResetTokenRepository;
+    }
+
+    @Override
+    public PasswordResetTokenEntity findByToken(String token) throws PasswordResetTokenNotFoundException {
+        PasswordResetTokenEntity entity = passwordResetTokenRepository.findByToken(token);
+        if (entity == null) {
+            throw new PasswordResetTokenNotFoundException(String.format("The password reset token %s doesn't exist", token));
+        }
+        return entity;
+    }
+
+    @Override
+    public PasswordResetTokenEntity findByAccountEntity(AccountEntity accountEntity) throws PasswordResetTokenNotFoundException {
+        PasswordResetTokenEntity entity = passwordResetTokenRepository.findByAccountEntity(accountEntity);
+        if (entity == null) {
+            throw new PasswordResetTokenNotFoundException(String.format("The password reset token for username %s doesn't exist", accountEntity.getLogin()));
+        }
+        return entity;
+    }
+
+    @Override
+    public PasswordResetTokenEntity save(PasswordResetTokenEntity entity) {
+        return null;
+    }
+}
