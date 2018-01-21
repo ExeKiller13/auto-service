692e54e8cf1028dc1e7f279097e3c9ab9d9175de
Alokhin <alokhin.kirill@gmail.com>
2018-01-21 15:17:23 +0200

    added verification token not found exception

---
+++ src/main/java/com/alokhin/autoservice/exception/VerificationTokenNotFoundException.java
@@ -0,0 +1,8 @@
+package com.alokhin.autoservice.exception;
+
+public class VerificationTokenNotFoundException extends Exception {
+
+    public VerificationTokenNotFoundException(String message) {
+        super(message);
+    }
+}
