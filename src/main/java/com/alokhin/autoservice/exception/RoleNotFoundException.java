bf813d3c83076416b2451d9eacd0c196f2ae96bb
Alokhin <alokhin.kirill@gmail.com>
2018-02-03 15:50:55 +0200

    added role not found exception

---
+++ src/main/java/com/alokhin/autoservice/exception/RoleNotFoundException.java
@@ -0,0 +1,8 @@
+package com.alokhin.autoservice.exception;
+
+public class RoleNotFoundException extends Exception {
+
+    public RoleNotFoundException(String message) {
+        super(message);
+    }
+}
